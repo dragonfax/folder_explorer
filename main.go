53a470dc1038855c4b83bafd1a742f8797e3b5d2
Jason Stillwell <jstillwell@zendesk.com>
2016-11-14 14:56:22 -0800

    .

--- main.go
+++ main.go
@@ -1,31 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
 )
 
+const RowsCount = 12
+const ColsCount = 3
+
 func main() {
 	widgets.NewQApplication(len(os.Args), os.Args)
 
-	//create a button and connect the clicked signal
-	/*var button = widgets.NewQPushButton2("Click me!", nil)
-	button.ConnectClicked(func(flag bool) {
-		widgets.QMessageBox_Information(nil, "OK", "You clicked me!", widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
-	}) */
+	// background window
+	bgWindow := widgets.NewQMdiSubWindow(nil, core.Qt__FramelessWindowHint|core.Qt__WindowStaysOnBottomHint)
+	// bgWindow.ShowMaximized()
 
-	mdiArea2 := widgets.NewQMdiArea(nil)
-	mdiWindow2 := widgets.NewQMdiSubWindow(nil, 0)
-	mdiWindow2.SetMinimumSize2(100, 100)
-	mdiArea2.AddSubWindow(mdiWindow2, 0)
+	// table list
+	table := widgets.NewQTableWidget2(RowsCount, ColsCount, nil)
+	table.SetShowGrid(true)
+	table.SetHorizontalHeaderLabels([]string{"ID", "Name", "Date"})
+	table.VerticalHeader().SetVisible(false)
+	for row := 1; row <= RowsCount; row++ {
+		for col := 1; col <= ColsCount; col++ {
+			var item *widgets.QTableWidgetItem
+			if col == 1 {
+				item = widgets.NewQTableWidgetItem2(fmt.Sprintf("%d", row), 0)
+			} else if col == 2 {
+				item = widgets.NewQTableWidgetItem2("Test", 0)
+			} else if col == 3 {
+				item = widgets.NewQTableWidgetItem2(time.Now().Format(time.Stamp), 0)
+			}
 
+			table.SetItem(row, col, item)
+		}
+	}
+
+	bgWindow.SetWidget(table)
+
+	// child folder window
+	cWindow := widgets.NewQMdiSubWindow(nil, 0)
+	cWindow.SetMinimumSize2(100, 100)
+
+	// mdi for parent folder
 	mdiArea := widgets.NewQMdiArea(nil)
-	mdiWindow := widgets.NewQMdiSubWindow(nil, 0)
-	mdiWindow.SetMinimumSize2(300, 300)
-	mdiWindow.SetWidget(mdiArea2)
-	mdiArea.AddSubWindow(mdiWindow, 0)
+	mdiArea.AddSubWindow(bgWindow, 0)
+	mdiArea.AddSubWindow(cWindow, 0)
+	bgWindow.Lower()
+	cWindow.Raise()
 
 	var layout = widgets.NewQVBoxLayout()
 	layout.AddWidget(mdiArea, 0, core.Qt__AlignCenter)
@@ -37,6 +62,10 @@ func main() {
 	window.Layout().DestroyQObject()
 	window.SetLayout(layout)
 	window.Show()
+
+	h := mdiArea.Viewport().Height()
+	w := mdiArea.Viewport().Width()
+	bgWindow.Resize2(w, h)
 
 	widgets.QApplication_Exec()
 }
